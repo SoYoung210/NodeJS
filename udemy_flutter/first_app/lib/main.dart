94bb97fdfc001800355e98b5cd9376096e28d0bb
SoYoung210 <ethdud1@gmail.com>
2018-10-24 00:16:57 +0900

    setState(() {}) 를 통한 앱 상태변경

--- udemy_flutter/first_app/lib/main.dart
+++ udemy_flutter/first_app/lib/main.dart
@@ -2,23 +2,43 @@ import 'package:flutter/material.dart';
 
 main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
-  @override
+class MyApp extends StatefulWidget {
+  State<StatefulWidget> createState() {
+    return _MyAppState();
+  }
+}
+
+class _MyAppState extends State<MyApp> {
+  List<String> products = ['Food Tester'];
   Widget build(context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          title: Text('data'),
-        ),
-        body: Card(
-          child: Column(
-            children: <Widget>[
-              Image.asset('assets/food.jpg'),
-              Text('Food Paradise')
-            ],
+          appBar: AppBar(
+            title: Text('data'),
           ),
-        ),
-      ),
+          body: Column(children: [
+            Container(
+                margin: EdgeInsets.all(10.0),
+                child: RaisedButton(
+                    onPressed: () {
+                      setState(() {
+                        products.add('Advanced');
+                      });
+                    },
+                    child: Text('Add Product'))),
+            Column(
+              children: products
+                  .map((element) => Card(
+                        child: Column(
+                          children: <Widget>[
+                            Image.asset('assets/food.jpg'),
+                            Text(element)
+                          ],
+                        ),
+                      ))
+                  .toList(),
+            )
+          ])),
     );
   }
 }
